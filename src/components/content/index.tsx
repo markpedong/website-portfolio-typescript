911850018f0cd63ee5247031a8ae3d63a02c2424
atlas <atlas.wbridges@gmail.com>
2024-04-02 16:44:23 +0800

    no message

--- src/components/content/index.tsx
+++ src/components/content/index.tsx
@@ -1,8 +1,10 @@
 import classNames from 'classnames'
 import styles from './styles.module.scss'
 import { Inter, Plus_Jakarta_Sans } from 'next/font/google'
+import { FaGithub, FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa6'
 
 const inter = Inter({ weight: '500', subsets: ['latin'] })
+const interR = Inter({ weight: '400', subsets: ['latin'] })
 const jakarta = Plus_Jakarta_Sans({ weight: '700', subsets: ['latin'] })
 
 const Content = () => {
@@ -16,8 +18,16 @@ const Content = () => {
 				<span className={jakarta.className}>
 					Lorem, <p style={{ color: '#7E74F1' }}>ipsum.</p>
 				</span>
-				<span>3</span>
-				<div>4</div>
+				<span className={interR.className}>
+					Create front-end developer with more than +2 years of experience in enterprise companies. Proficient
+					in Sass, NextJS and JavaScript. Passionate about Golang.
+				</span>
+				<div>
+					<FaInstagram />
+					<FaGithub />
+					<FaTwitter />
+					<FaLinkedin />
+				</div>
 			</div>
 			<div>2</div>
 		</div>
