4b49b089f8e244fd2d63f43a0c97e6b9dea8899b
atlas <atlas.wbridges@gmail.com>
2024-04-02 17:32:05 +0800

    no message

--- src/components/specialty/index.tsx
+++ src/components/specialty/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import styles from './styles.module.scss'
+import Header from '../header'
 
 const Specialty = () => {
-  return (
-    <div>Specialty</div>
-  )
+	return (
+		<div className={styles.specialtyWrapper}>
+			<Header title="services" />
+		</div>
+	)
 }
 
 export default Specialty
