013312f39427f49ea4f11483fc975cd33d6d5ac6
atlas <atlas.wbridges@gmail.com>
2024-04-07 18:46:05 +0800

    no message

--- server/main.go
+++ server/main.go
@@ -4,6 +4,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func CorsMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
 func main() {
 	r := gin.New()
+
+	r.Use(CorsMiddleware())
 }
