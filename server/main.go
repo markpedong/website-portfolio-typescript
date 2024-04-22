1b35969f3506fa5c1a8e778944513be8597da76b
atlas <atlas.wbridges@gmail.com>
2024-04-22 11:17:08 +0800

    no message

--- server/main.go
+++ server/main.go
@@ -16,7 +16,7 @@ func init() {
 }
 func CorsMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		allowedOrigins := []string{"https://markpedong.com", "https://website-portfolio-admin.vercel.app"}
+		allowedOrigins := []string{"https://markpedong.com", "https://website-portfolio-typescript-ruby.vercel.app"}
 		origin := ctx.Request.Header.Get("Origin")
 
 		for _, allowedOrigin := range allowedOrigins {
