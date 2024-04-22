c57fc11d0229169501cb89dbb38dc711fd47abd9
atlas <atlas.wbridges@gmail.com>
2024-04-22 14:12:48 +0800

    - fix error.

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -1,5 +1,5 @@
 import Root from '@/pages/root'
-import '@/styles/global.module.scss'
+import '@/styles/global.scss'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
