1b1c01e3d5cca830e147589180636076bef1a7b3
markpedong <mark.pedong01@gmail.com>
2024-04-29 20:35:03 +0800

    no message

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google'
 import '../styles/global.scss'
 import ReduxProvider from '@/redux/provider'
 import { Toaster } from '@/components/ui/toaster'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +23,8 @@ export default function RootLayout({
 			<body className={inter.className}>
 				<ReduxProvider>{children}</ReduxProvider>
 				<Toaster />
+				<SpeedInsights />
+				<Analytics />
 			</body>
 		</html>
 	)
